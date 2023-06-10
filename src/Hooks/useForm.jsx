daa367db286c611bb3b81dfda34a7c5eec886272
LucasDev <LucasDev9645@gmail.com>
2023-06-10 11:01:28 -0300

    Login create

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 
-const useForm = () => {
+const types = {
+  email: {
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: "Preencha um email válido",
+  },
+};
+
+const useForm = (type) => {
   const [value, setValue] = useState("");
   const [error, setError] = useState(null);
 
@@ -10,8 +18,12 @@ const useForm = () => {
   }
 
   function validate(value) {
+    if (type === false) return true;
     if (value.length === 0) {
       setError("Preencha o campo.");
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
       return false;
     } else {
       setError(null);
