5608b7eaaef570fbbc84e5d4f50a4c3fadfff49b
LucasDev <LucasDev9645@gmail.com>
2023-06-11 11:01:52 -0300

    Create useFetch

--- src/components/Login/LoginCreate/index.jsx
+++ src/components/Login/LoginCreate/index.jsx
@@ -3,7 +3,8 @@ import { useContext } from "react";
 import { USER_POST } from "../../../util/api";
 import useForm from "../../../Hooks/useForm";
 import { UserContext } from "../../../UserContext";
-
+import useFetch from "../../../Hooks/useFetch";
+import Error from "../../../Help/Error";
 import Input from "../../Reusable/Input";
 import Button from "../../Reusable/Button";
 
@@ -13,6 +14,7 @@ const LoginCreate = () => {
   const password = useForm();
 
   const { userLogin } = useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -21,7 +23,7 @@ const LoginCreate = () => {
       email: email.value,
       password: password.value,
     });
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) userLogin(username.value, password.value);
   }
 
@@ -32,7 +34,12 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );
